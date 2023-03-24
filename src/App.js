4aebe8b852115c4ab72d493a9256d160a7185d9f
Lev_Gordon <lev.gordonsa@gmail.com>
2023-03-24 14:20:27 -0700

    second tutorial screen complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import navbar from './photos/navbar.png'
 import leftbar from './photos/leftbar.png'
 import herdman from './photos/Herdman.jpg'
 import website from './photos/website.png'
+import telephone from './photos/telephone.png'
+import FAQ from './photos/FAQ.png'
+
 import { useState } from 'react';
 
 function App() {
@@ -35,15 +38,30 @@ function App() {
 
   const secondScreen = (
     <div className='secondScreen'>
-      <div className='schedule-call'>
+      <div className='secondScreen-triblock'>
+      <div className='schedule-call box'>
+        <img src={telephone} className="icon" alt='telephone'/>
+        <p>Schedule a FREE 30 minute explanatory call with John Herdman.</p>
+        <a href="https://calendly.com/herdmanhealth/" target="_blank" rel="noreferrer"><button>SCHEDULE NOW</button></a>
+      </div>
+      <div className='watch-video box'>
+        <iframe src="https://www.youtube.com/embed/oXNKr3-dIcM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <p> Watch a video tutorial and start your first assessment.</p>
+        <a href="https://www.youtube.com/watch?v=oXNKr3-dIcM&ab_channel=HerdmanHealth" target="_blank" rel="noreferrer"><button>WATCH VIDEO</button></a> 
+      </div>
+      <div className='FAQ box'>
+        <img src={FAQ} className="icon" alt='FAQ Herdman' />
+        <p>Take a look at our frequently asked questions.</p> 
+         <button>FAQ</button>
 
       </div>
-      <div className='watch-video'>
 
       </div>
-      <div className='FAQ'>
+        <div className='button-div'>
+          <button>PROCEED TO HERDMANHEALTH</button>
+        </div>
 
-      </div>
+
 
     </div>
   )
