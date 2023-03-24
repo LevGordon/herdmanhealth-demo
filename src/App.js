59110fd70c9920aee373e80f7a39c46ecbde5563
Lev_Gordon <lev.gordonsa@gmail.com>
2023-03-24 13:09:28 -0700

    final first page design and useState for tutorial display

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import navbar from './photos/navbar.png'
 import leftbar from './photos/leftbar.png'
 import herdman from './photos/Herdman.jpg'
+import website from './photos/website.png'
 import { useState } from 'react';
 
 function App() {
 
   const [currentPage, setCurrentPage] = useState("first")
+  const [tutorialDisplayed, setTutorialDisplayed] = useState(true)
 
   const firstScreen = (
     <div className='firstScreen'>
@@ -15,6 +17,7 @@ function App() {
           <p> Dr. John Herdman from HerdmanHealth</p>
           <h2> Hi, I'm John from HerdmanHealth. Thank you for deciding to go forward with our app. </h2>
           <h2> I'm here to help you use this app and streamline your assessments.</h2>
+          <h2> There are a few ways that I can help you get an understanding of how to use it, press "get started!" once you are ready to proceed.</h2>
         </div>
         <div>
           <img className="herdman-pic" src={herdman} alt="John Herdman"/>
@@ -24,14 +27,23 @@ function App() {
       
       
       
-      <button type='button' className='get-started-button' onClick={() => setCurrentPage("second")}> Get Started! </button>
+      <button type='button' onClick={() => setCurrentPage("second")}> Get Started! </button>
 
     </div>
 
   )
 
   const secondScreen = (
-    <div>
+    <div className='secondScreen'>
+      <div className='schedule-call'>
+
+      </div>
+      <div className='watch-video'>
+
+      </div>
+      <div className='FAQ'>
+
+      </div>
 
     </div>
   )
@@ -44,9 +56,9 @@ function App() {
         <img className="website-navbar" src={navbar} alt="herdman navbar"/>
         <div className='leftbar-content'>
           <img className="website-leftbar" src={leftbar} alt="herdman leftbar"/>
-          <div className='content-box'>
+          { tutorialDisplayed === true ? (<div className='content-box'>
             { currentPage === "first" ? firstScreen : secondScreen }
-          </div>
+          </div>) : <img src={website} alt="typical herdmanhealth website look"/>}
         </div>
         
       </header>
