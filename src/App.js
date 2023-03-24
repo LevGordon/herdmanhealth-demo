e50350ca12091ac915240585254114908bb2ab25
Lev_Gordon <lev.gordonsa@gmail.com>
2023-03-23 20:31:54 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import navbar from './photos/navbar.png'
+import leftbar from './photos/leftbar.png'
+import herdman from './photos/Herdman.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className="website-navbar" src={navbar} alt="herdman navbar"/>
+        <div className='leftbar-content'>
+          <img className="website-leftbar" src={leftbar} alt="herdman leftbar"/>
+          <div className='content-box'>
+            <img className="herdman-pic" src={herdman} alt="John Herdman"/>
+          </div>
+        </div>
+        
       </header>
     </div>
   );
