d543882ec1aeb346d3463ef29785108496096ec3
Lev_Gordon <lev.gordonsa@gmail.com>
2023-03-23 21:46:07 -0700

    first screen

--- src/App.js
+++ src/App.js
@@ -2,8 +2,42 @@ import './App.css';
 import navbar from './photos/navbar.png'
 import leftbar from './photos/leftbar.png'
 import herdman from './photos/Herdman.jpg'
+import { useState } from 'react';
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState("first")
+
+  const firstScreen = (
+    <div className='firstScreen'>
+      <div className='firstScreen-textandpic'> 
+        <div className='intro-textbox'>
+          <p> Dr. John Herdman from HerdmanHealth</p>
+          <h2> Hi, I'm John from HerdmanHealth. Thank you for deciding to go forward with our app. </h2>
+          <h2> I'm here to help you use this app and streamline your assessments.</h2>
+        </div>
+        <div>
+          <img className="herdman-pic" src={herdman} alt="John Herdman"/>
+        </div>
+
+      </div>
+      
+      
+      
+      <button type='button' className='get-started-button' onClick={() => setCurrentPage("second")}> Get Started! </button>
+
+    </div>
+
+  )
+
+  const secondScreen = (
+    <div>
+
+    </div>
+  )
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,7 +45,7 @@ function App() {
         <div className='leftbar-content'>
           <img className="website-leftbar" src={leftbar} alt="herdman leftbar"/>
           <div className='content-box'>
-            <img className="herdman-pic" src={herdman} alt="John Herdman"/>
+            { currentPage === "first" ? firstScreen : secondScreen }
           </div>
         </div>
         
